339f37e2940213ea683e56aad683a46a8c64a2bf
금가경 <gageum0@gmail.com>
2025-08-25 01:22:31 +0900

    extract to viwModel which change message when view appear

--- Tamagotchi/Main/MainViewController.swift
+++ Tamagotchi/Main/MainViewController.swift
@@ -54,16 +54,16 @@ final class MainViewController: BaseViewController {
     private let meal = BehaviorRelay(value: UserDefaults.standard.string(forKey: .meal))
     private let water = BehaviorRelay(value: UserDefaults.standard.string(forKey: .water))
     
-    private lazy var message = BehaviorRelay(value: talks.randomElement()!)
-    private var talks: [String] {
-        let userName = name.value
-        return [
-            "복습 아직 안하셨다구요? 지금 잠이 오세여? \(userName)님??",
-            "테이블뷰컨트롤러와 뷰컨트롤러는 어떤 차이가 있을까요? \(userName)님?",
-            "\(userName)님 오늘 깃허브 푸시 하셨어영?",
-            "\(userName)님!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-        ]
-    }
+//    private lazy var message = BehaviorRelay(value: talks.randomElement()!)
+//    private var talks: [String] {
+//        let userName = name.value
+//        return [
+//            "복습 아직 안하셨다구요? 지금 잠이 오세여? \(userName)님??",
+//            "테이블뷰컨트롤러와 뷰컨트롤러는 어떤 차이가 있을까요? \(userName)님?",
+//            "\(userName)님 오늘 깃허브 푸시 하셨어영?",
+//            "\(userName)님!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+//        ]
+//    }
     
     private let viewModel: MainViewModel
     
@@ -145,6 +145,10 @@ final class MainViewController: BaseViewController {
             .bind(to: navigationItem.rx.title)
             .disposed(by: disposeBag)
         
+        output.message
+            .bind(to: messageLabel.rx.text)
+            .disposed(by: disposeBag)
+        
         navigationItem.rightBarButtonItem?.rx.tap
             .asDriver()
             .drive(with: self) { owner, _ in
@@ -152,10 +156,7 @@ final class MainViewController: BaseViewController {
                 owner.navigationController?.pushViewController(vc, animated: true)
             }
             .disposed(by: disposeBag)
-        
-        message
-            .bind(to: messageLabel.rx.text)
-            .disposed(by: disposeBag)
+
         
         // TODO: - self 떼내기
         // TODO: - refactoring
@@ -187,28 +188,28 @@ final class MainViewController: BaseViewController {
             .subscribe(onNext: { UserDefaults.standard.set($0, forKey: .name) })
             .disposed(by: disposeBag)
         
-        mealView.feedButton.rx.tap
-            .withLatestFrom(mealView.textField.rx.text.orEmpty)
-            .filter { $0.allSatisfy { $0.isNumber } }
-            .compactMap { Int($0) ?? 1 }
-            .filter { $0 < 100 }
-            .subscribe(with: self) { owner, add in
-                let count = Int(owner.meal.value ?? "0")! + add
-                owner.meal.accept("\(count)")
-                owner.messageLabel.text = owner.talks.randomElement()!
-            }
-            .disposed(by: disposeBag)
+//        mealView.feedButton.rx.tap
+//            .withLatestFrom(mealView.textField.rx.text.orEmpty)
+//            .filter { $0.allSatisfy { $0.isNumber } }
+//            .compactMap { Int($0) ?? 1 }
+//            .filter { $0 < 100 }
+//            .subscribe(with: self) { owner, add in
+//                let count = Int(owner.meal.value ?? "0")! + add
+//                owner.meal.accept("\(count)")
+//                owner.messageLabel.text = owner.talks.randomElement()!
+//            }
+//            .disposed(by: disposeBag)
 
-        waterView.feedButton.rx.tap
-            .withLatestFrom(waterView.textField.rx.text.orEmpty)
-            .filter { $0.allSatisfy { $0.isNumber } }
-            .compactMap { Int($0) ?? 1 }
-            .filter { $0 < 50 }
-            .subscribe(with: self) { owner, add in
-                let count = Int(owner.water.value ?? "0")! + add
-                owner.water.accept("\(count)")
-                owner.messageLabel.text = owner.talks.randomElement()!
-            }
-            .disposed(by: disposeBag)
+//        waterView.feedButton.rx.tap
+//            .withLatestFrom(waterView.textField.rx.text.orEmpty)
+//            .filter { $0.allSatisfy { $0.isNumber } }
+//            .compactMap { Int($0) ?? 1 }
+//            .filter { $0 < 50 }
+//            .subscribe(with: self) { owner, add in
+//                let count = Int(owner.water.value ?? "0")! + add
+//                owner.water.accept("\(count)")
+//                owner.messageLabel.text = owner.talks.randomElement()!
+//            }
+//            .disposed(by: disposeBag)
     }
 }
--- Tamagotchi/Main/MainViewModel.swift
+++ Tamagotchi/Main/MainViewModel.swift
@@ -17,9 +17,11 @@ final class MainViewModel: InputOutput {
     struct Output {
         let tamagotchi: Observable<Tamagotchi>
         let title: Observable<String>
+        let message: Observable<String>
     }
     
     private let tamagotchi: Tamagotchi
+    private let userName = UserDefaults.standard.string(forKey: .name) ?? "대장"
     
     init(tamagotchi: Tamagotchi) {
         self.tamagotchi = tamagotchi
@@ -29,17 +31,25 @@ final class MainViewModel: InputOutput {
         let tamagotchi = Observable.just(tamagotchi)
         
         let title = input.viewWillAppear
-            .map { "\(UserDefaults.standard.string(forKey: .name) ?? "대장")님의 다마고치"}
+            .withUnretained(self)
+            .map { "\($0.0.userName)님의 다마고치"}
         
-        return Output(tamagotchi: tamagotchi, title: title)
+        let message = input.viewWillAppear
+            .withUnretained(self)
+            .map { $0.0.randomMessage() }
+            
+        return Output(tamagotchi: tamagotchi, title: title, message: message)
     }
     
-//    name
-//        .compactMap { $0 }
-//        .map { "\($0)님의 다마고치" }
-//        .bind(to: navigationItem.rx.title)
-//        .disposed(by: disposeBag)
-//    
+    private func randomMessage() -> String {
+        return [
+            "복습 아직 안하셨다구요? 지금 잠이 오세여? \(userName)님??",
+            "테이블뷰컨트롤러와 뷰컨트롤러는 어떤 차이가 있을까요? \(userName)님?",
+            "\(userName)님 오늘 깃허브 푸시 하셨어영?",
+            "\(userName)님!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+        ].randomElement()!
+    }
+
 //    navigationItem.rightBarButtonItem?.rx.tap
 //        .asDriver()
 //        .drive(with: self) { owner, _ in
